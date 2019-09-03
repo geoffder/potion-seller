0defb608a1a07f38263f70547af7b55f168f970b
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-09-03 11:32:04 -0700

    remove unused lines in misc sftp testing block

--- discord_bot/test/azazel_test.exs
+++ discord_bot/test/azazel_test.exs
@@ -6,9 +6,10 @@ defmodule Bot.AzazelTest do
     # {:ok, handle} = :ssh_sftp.opendir(pid, 'Video1')
     # IO.puts(inspect(handle))
 
-    path = 'Video1/Action/Movies/James Bond 007'
     IO.puts(inspect(:ssh_sftp.list_dir(pid, '')))
-    # task = Task.async(fn -> :ssh_sftp.list_dir(pid, path) end)
-    # IO.puts(inspect(Task.await(task, 50_000)))
+
+    path = 'Video1/Action/Movies/James Bond 007'
+    task = Task.async(fn -> :ssh_sftp.list_dir(pid, path) end)
+    IO.puts(inspect(Task.await(task, 50_000)))
   end
 end
